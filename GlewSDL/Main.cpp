cf66429b3a772518eb6c64e788ad4d5a620378bc
k3yro <zeilenfang@gmail.com>
2019-05-18 11:50:34 +0200

    Standardwerte fuer Framebuffer gesetzt

--- GlewSDL/Main.cpp
+++ GlewSDL/Main.cpp
@@ -15,6 +15,14 @@ int main(int argc, char** argv)
 	SDL_Window* window;
 	SDL_Init(SDL_INIT_EVERYTHING);
 
+	//Aufbau FrameBuffer(4x8 -> 1Pixel = 32bit):
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8/*bit*/);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8/*bit*/);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8/*bit*/);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8/*bit*/);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32/*bit*/);	// Optional (minimale Buffergroesse)
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1/*An*/);	// Doppelpufferung einschalten
+
 	window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL/*Typ - z.B. Vulcan usw.*/);
 	
 	//Fehlerkorrektur
@@ -26,8 +34,13 @@ int main(int argc, char** argv)
 
 	bool close = false;
 
-	while (!close)
+	while (!close) // GameLoop
 	{
+		//Zeichnen:
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // Alten Buffer loeschen
+		glClear(GL_COLOR_BUFFER_BIT);
+		SDL_GL_SwapWindow(window); //2 Buffer (Monitorausgabe|Bildberechnung) -> Doppelpufferung
+
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
