f2116f8b0478f53806ff5ec16b83404748f098ac
k3yro <zeilenfang@gmail.com>
2019-05-19 14:57:55 +0200

    Vollbild, Zeitmessung, Wirefram Modus

--- GlewSDL/Main.cpp
+++ GlewSDL/Main.cpp
@@ -28,7 +28,11 @@ int main(int argc, char** argv)
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32/*bit*/);	// Optional (minimale Buffergroesse)
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1/*An*/);	// Doppelpufferung einschalten
 
-	window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL/*Typ - z.B. Vulcan usw.*/);
+	//uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP;
+	//uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP;
+	uint32_t flags = SDL_WINDOW_OPENGL;
+
+	window = SDL_CreateWindow("First Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, flags);
 	
 	//Todo: Fehlerkorrektur window
 
@@ -64,6 +68,15 @@ int main(int argc, char** argv)
 	Shader shader("basic.vs.txt", "basic.fs.txt");
 	shader.bind();
 
+	//Zeit messen:
+	uint64_t perfCounterFrequency = SDL_GetPerformanceFrequency();
+	uint64_t lastCounter = SDL_GetPerformanceCounter();
+	float delta = 0.0f;
+
+	//Wireframe Modus
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // Wireframe
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // Normal
+
 	bool close = false;
 	while (!close) // GameLoop
 	{
@@ -85,6 +98,14 @@ int main(int argc, char** argv)
 				close = true;
 			}
 		}
+
+		//Zeit messen:
+		uint64_t endCounter = SDL_GetPerformanceCounter();
+		uint64_t counterElapsed = endCounter - lastCounter;
+		delta = ((float)counterElapsed) / (float)perfCounterFrequency;
+		uint32_t FPS = (uint32_t)((float)perfCounterFrequency / (float)counterElapsed);
+		std::cout << FPS << std::endl;
+		lastCounter = endCounter;
 	}
 	return 0;
 }
