bc79a2ad8a228d451f802e27b7129a3e969d1368
k3yro <zeilenfang@gmail.com>
2019-05-19 18:06:54 +0200

    OpenGL Fehlerbehandlung

--- GlewSDL/Main.cpp
+++ GlewSDL/Main.cpp
@@ -3,6 +3,8 @@
 #include <GL/glew.h>
 #define SDL_MAIN_HANDLED	//Eigene Main Funktion verwenden
 #include <SDL.h>
+#include <Windows.h>
+#include <sstream>
 
 // Linker - Eingabe - Zusaetzliche Abhaengigkeiten
 #pragma comment(lib, "SDL2.lib")
@@ -10,8 +12,43 @@
 #pragma comment(lib, "opengl32.lib")
 
 #include "vertex_buffer.h"
+#include "index_buffer.h"
 #include "defines.h"
 #include "shader.h"
+
+// Neue OpenGl Debug Variante:
+std::string lastErrorMessage = "";
+void APIENTRY openGLDebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
+	//if (severity == GL_DEBUG_SEVERITY_HIGH || severity == GL_DEBUG_SEVERITY_MEDIUM)
+	//{
+	if (lastErrorMessage != (std::string)message)
+	{
+		lastErrorMessage = (std::string)message;
+		std::cout << "[OpenGL_New Error] " << message << std::endl;
+	}
+	//}	
+}
+
+// Alte OpenGl Debug Variante (GLCALL):
+#ifdef _DEBUG
+std::string lastErrorFile = "";
+void _GLGetError(const char* file, int line, const char* call) {
+	std::stringstream ss;
+	ss << file << line;
+	if (ss.str() != lastErrorFile)
+	{
+		lastErrorFile = ss.str();
+		while (GLenum error = glGetError()) {
+			std::cout << "[OpenGL_Old Error] " << glewGetErrorString(error) << " in " << file << ":" << line << " Call: " << call << std::endl;
+		}
+
+	}
+	ss.str(std::string());
+}
+#define GLCALL(call) call; _GLGetError(__FILE__, __LINE__, #call)
+#else
+#define GLCALL(call) call
+#endif
 
 // OpenGL Befehle nachschlagen: http://docs.gl
 //TODO: Debug -> mit Konsole | Release -> nur Fenster
@@ -27,6 +64,11 @@ int main(int argc, char** argv)
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8/*bit*/);
 	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32/*bit*/);	// Optional (minimale Buffergroesse)
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1/*An*/);	// Doppelpufferung einschalten
+
+#ifdef _DEBUG
+	// Spezieller Debug Mode (Verlangsamung)
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+#endif
 
 	//uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS | SDL_WINDOW_ALWAYS_ON_TOP;
 	//uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP;
@@ -51,6 +93,12 @@ int main(int argc, char** argv)
 	}
 	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 
+#ifdef _DEBUG
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageCallback(openGLDebugCallback, 0);
+#endif
+
 	//Daten fuer Dreieck:
 	Vertex verticies[] = {
 		Vertex{-0.5f, -0.5f, 0.0f,
@@ -63,6 +111,15 @@ int main(int argc, char** argv)
 				1.0f, 0.0f, 0.0f, 1.0f}
 	};
 	uint32_t countVerticies = 4; // Anzahl Dreiecke in verticies Array
+
+	//Index fuer komplexere Formen
+	uint32_t indices[] = {
+		0, 1, 2,
+		1, 2, 3
+	};
+	uint32_t numIndices = 6;
+
+	IndexBuffer indexBuffer(indices, numIndices, sizeof(indices[0]));
 
 	VertexBuffer vertexBuffer(verticies, countVerticies);
 	vertexBuffer.Unbind();
@@ -87,7 +144,9 @@ int main(int argc, char** argv)
 
 		
 		vertexBuffer.Bind();
-		glDrawArrays(GL_TRIANGLE_STRIP, 0, countVerticies);
+		indexBuffer.bind();
+		GLCALL(glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, 0));
+		indexBuffer.unbind();
 		vertexBuffer.Unbind();
 
 		SDL_GL_SwapWindow(window); //2 Buffer (Monitorausgabe|Bildberechnung) -> Doppelpufferung
